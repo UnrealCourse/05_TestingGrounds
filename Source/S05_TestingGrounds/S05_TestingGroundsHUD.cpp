16b1574e75e513dfd9a51f1c343590fb9d1d78ed
Markyroson <Markyroson@users.noreply.github.com>
2016-11-29 13:56:05 -0800

    Modified casing of comments to create consistency

--- Source/S05_TestingGrounds/S05_TestingGroundsHUD.cpp
+++ Source/S05_TestingGrounds/S05_TestingGroundsHUD.cpp
@@ -20,14 +20,14 @@ void AS05_TestingGroundsHUD::DrawHUD()
 
 	// Draw very simple crosshair
 
-	// find center of the Canvas
+	// Find center of the Canvas
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
-	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
+	// Offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
 	const FVector2D CrosshairDrawPosition( (Center.X),
 										   (Center.Y));
 
-	// draw the crosshair
+	// Draw the crosshair
 	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
 	TileItem.BlendMode = SE_BLEND_Translucent;
 	Canvas->DrawItem( TileItem );
