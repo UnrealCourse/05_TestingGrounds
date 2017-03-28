94a47395d465f9d55259b59b6dc3fc7426e959fd
Samuel Pattuzzi <sam@pattuzzi.it>
2017-03-28 18:57:28 +0100

    80_TG_URC Animation Montages and Slots

--- Source/S05_TestingGrounds/Character/Mannequin.cpp
+++ Source/S05_TestingGrounds/Character/Mannequin.cpp
@@ -37,7 +37,7 @@ void AMannequin::BeginPlay()
 	}
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
-	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	Gun->AnimInstance = GetMesh()->GetAnimInstance();
 
 	if (InputComponent != NULL) {
 		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
