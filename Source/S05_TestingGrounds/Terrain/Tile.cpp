934720f2725438e8beda65c1b508860c865e10ec
Samuel Pattuzzi <sam@pattuzzi.it>
2017-04-05 18:37:43 +0100

    86_TG_URC Configure Custom Trace Channels

--- Source/S05_TestingGrounds/Terrain/Tile.cpp
+++ Source/S05_TestingGrounds/Terrain/Tile.cpp
@@ -50,10 +50,10 @@ bool ATile::CastSphere(FVector Location, float Radius)
 		Location,
 		Location,
 		FQuat::Identity,
-		ECollisionChannel::ECC_Camera,
+		ECollisionChannel::ECC_GameTraceChannel2,
 		FCollisionShape::MakeSphere(Radius)
 	);
 	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
-	DrawDebugSphere(GetWorld(), Location, Radius, 100, ResultColor, true, 100);
+	DrawDebugCapsule(GetWorld(), Location, 0, Radius, FQuat::Identity, ResultColor, true, 100);
 	return HasHit;
 }
